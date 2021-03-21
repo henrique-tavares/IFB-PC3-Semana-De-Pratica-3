7230c2eb3c72562bd113a64201131c9d7ba12747
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2021-03-21 13:57:26 -0300

    updating github repository link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <footer>
             <p>Desenvolvido por <strong>Henrique Tavares Aguiar</strong></p>
-            <a href="https://github.com/henrique-tavares">
+            <a href="https://github.com/henrique-tavares/IFB-PC3-Semana-De-Pratica-3">
                 <img src={ githubImg } alt="GitHub" />
             </a>
         </footer>
